ddf6a1fce90867610ce58243ba1fa299222f82e1
Vasanthasenan R <vasanthsenan333@gmail.com>
2026-05-21 08:32:56 +0530

    Practices

---
+++ Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace practiceforundsd
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        string username = "Admin";
+        string password = "vk0409";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtusername.Text == username && txtpassword.Password == password)
+            {
+                Logic  ohome = new Logic ();
+                ohome.Show();
+            }
+               
+            else
+            {
+                MessageBox.Show("Ivalid Username And Password");
+            }
+        }
+        
+    }
+}
